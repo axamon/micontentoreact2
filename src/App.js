80e3a8dbfa1a1d9b02d4f6c80b820a80ab32562d
00246506 <alberto.bregliano@telecomitalia.it>
2022-05-10 23:40:31 +0200

    fiamma2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css';
-import ReactPlayer from 'react-player/lazy'
 import Title from './components/title/Title';
 import Argomento from './components/argomento/Argomento';
 import Footer from './components/footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <Title titolo="MiContento" />
-      <div className='main'>
-        <ReactPlayer  playing muted loop url={"https://s3.eu-south-1.amazonaws.com/bregliano.com/video.mp4"} />
+    <>
+      <div className="App">
+        <Title titolo="MiContento" />
       </div>
-      <Argomento name="Contatti" />
-      <Argomento name="Sportello" />
-      <Argomento name="GPT" />
+      
+      <div className='container'>
+        <div className='d-flex justify-content-center'>
+          <video id="myVideo" autostart loop muted autoPlay src={"https://s3.eu-south-1.amazonaws.com/bregliano.com/video.mp4"} type="video/mp4" />
+        </div>
+        <div className='argomenti row'>
+          <Argomento name="Contatti" contenuto="Contattaci solo il 31 novembre o il 30 febbraio"/>
+          <Argomento name="Sportello" contenuto="Sportello amico"/>
+          <Argomento name="GPT" contenuto="Non so cosa sia"/>
+        </div>
+      </div>
       <Footer owner="MiContento" />
-    </div>
+    </>
   );
 }
 
