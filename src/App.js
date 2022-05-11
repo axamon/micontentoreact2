dcb2c8b4bff04a16db5be3380a671131451dccda
00246506 <alberto.bregliano@telecomitalia.it>
2022-05-11 09:29:02 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -4,18 +4,25 @@ import './App.css';
 import Title from './components/title/Title';
 import Argomento from './components/argomento/Argomento';
 import Footer from './components/footer/Footer';
+import Typewriter from './components/typewriter/Typewriter';
 
 function App() {
   return (
     <>
+    <div className='video-wrapper'>
+          <video id="myVideo" autostart loop muted autoPlay src={"https://s3.eu-south-1.amazonaws.com/bregliano.com/video.mp4"} type="video/mp4" />
+          <div class="header">
+            <h1><Typewriter text='Unità di misura di senso.' /></h1>
+              {/* <button>Recipe here</button> */}
+          </div>
+        </div>
       <div className="App">
         <Title titolo="MiContento" />
       </div>
+    
       
       <div className='container'>
-        <div className='d-flex justify-content-center'>
-          <video id="myVideo" autostart loop muted autoPlay src={"https://s3.eu-south-1.amazonaws.com/bregliano.com/video.mp4"} type="video/mp4" />
-        </div>
+        
         <div className='argomenti row'>
           <Argomento name="Contatti" contenuto="Contattaci solo il 31 novembre o il 30 febbraio"/>
           <Argomento name="Sportello" contenuto="Sportello amico"/>
--- src/components/typewriter/Typewriter.jsx
+++ src/components/typewriter/Typewriter.jsx
@@ -5,7 +5,9 @@ export default class Typewriter extends Component {
   render() {
         const { text = "very long text!" } = this.props;
     return (
-      <div className='typewriter'>{text}</div>
+      <div>
+      <p class="line-1 anim-typewriter">{text}</p>
+      </div>
     )
   }
 }
